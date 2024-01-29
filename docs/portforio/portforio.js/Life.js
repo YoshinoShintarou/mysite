5a09804b11ed2e6ad6c7cb0e8d13aadc1215ccaf
Yoshino Shintarou <ys0726111@gmail.com>
2024-01-29 11:42:12 +0000

    修正

--- docs/portforio/portforio.js/Life.js
+++ docs/portforio/portforio.js/Life.js
@@ -6,3 +6,15 @@ $(function(){
         $('.start').fadeOut(500);
     },2500);
     });
+    $(function (){
+      $(window).scroll(function(){
+          const wHeight=$(window).height();
+          const WScroll=$(window).scrollTop();
+          $(".fadein-box").each(function(){
+              const bPosition=$(this).offset().top;
+              if( WScroll > bPosition-wHeight+200){
+                  $(this).addClass("action");
+              }
+          })
+      })
+  })
